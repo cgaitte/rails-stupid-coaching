01fb2f15da703f7e3d022e21028e0eb7cd833173
Cyril Gaitte <cyril.gaitte@gmail.com>
2019-05-16 14:08:06 +0200

    ex 1 done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+def ask
+end
+
+ def answer
+  @question = params[:userinput]
+  if @question.match(/\?\z/)
+    @answer = "Silly question, get dressed and go to work!"
+  elsif @question == "I am going to work"
+   @answer = "Great!"
+  else @answer = "I don't care, get dressed and go to work!"
+  end
+ end
 end
